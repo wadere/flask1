c00b80ed072eeb2b4ce3101e09ca4222ee7e362d
wadere <wadere@gmail.com>
2017-05-24 12:43:07 -0600

    update

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config.from_object('config')
+app_settings = app.config['APP_SETTINGS']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/wordcount_dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/wordcount_dev'
+
+db = SQLAlchemy(app)
+
+from models import *
 
 @app.route('/')
 def hello():
-    return "Hello you handsom devil you!"
+    return "Hello using postgres and flask-sqlalchemy!"
 
 
 @app.route('/')
