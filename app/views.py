416a35ad5cb9fb733607dfb106b9f1f649e3ed8f
wadere <wadere@gmail.com>
2017-05-24 17:37:32 -0600

    update

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import feedparser
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template/')
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -13,19 +13,11 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route('/<publication>')
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
-
-    first_article = feed['entries'][1]
-    return """<html>
-            <body>
-                <h1> {3} Most Recent </h1>
-                <b>{0}</br>
-                <i>{1}</br>
-                <p>{2}</br>
-            </body>
-            </html>""".format(first_article.get("title"),
-                              first_article.get('published'),
-                              first_article.get('summary'),
-                              publication.upper())
+    first_article = feed['entries'][0]
+    return render_template('template/home.html',
+                           title=first_article.get('title'),
+                           published=first_article.get('published'),
+                           summary=first_article.get('summary'))
 
 
 if __name__ == '__main__':
