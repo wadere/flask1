8a730b78fdbab109006ae2ab47e3297cd4ca779b
wadere <wadere@gmail.com>
2017-05-24 17:15:39 -0600

    update

--- app/views.py
+++ app/views.py
@@ -1,22 +1,32 @@
+import feedparser
+from flask import Flask
 from flask import render_template
-from app import app
 
+app = Flask(__name__)
+
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest'
+             }
 
 @app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Miguel'}
-    posts = [
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+@app.route('/<publication>')
+def get_news(publication='bbc'):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+
+    first_article = feed['entries'][1]
+    return """<html>
+            <body>
+                <h1> {3} Most Recent </h1>
+                <b>{0}</br>
+                <i>{1}</br>
+                <p>{2}</br>
+            </body>
+            </html>""".format(first_article.get("title"),
+                              first_article.get('published'),
+                              first_article.get('summary'),
+                              publication.upper())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
