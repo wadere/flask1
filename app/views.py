aa2c83caa5313b89f0a43efce07a9e15a673b8ab
wadere <wadere@gmail.com>
2017-05-24 19:40:01 -0600

    added jinja loop

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ import feedparser
 import os
 from flask import Flask
 from flask import render_template
+from flask import request
 
 import jinja2
 jinja_environment = jinja2.Environment(autoescape=True,
@@ -17,8 +18,12 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route('/')
 @app.route('/<publication>')
 def get_news(publication='bbc'):
+    query = request.args.get('publication')
+    if not query or query.lower() not in RRS_FEEDS:
+        publication='bbc'
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
     return render_template('home.html',
                            articles=feed['entries'],source=publication.upper())
 
