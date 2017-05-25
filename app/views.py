5d88240c25687287b04f53a514ede67082966192
wadere <wadere@gmail.com>
2017-05-24 21:20:16 -0600

    update

--- app/views.py
+++ app/views.py
@@ -16,20 +16,39 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest'}
 
 DEFAULTS = {'publication':'bbc',
-            'city':'London, UK'}
+            'city':'London, UK',
+            'currency_from': 'GBP',
+            'currency_to':'USD'}
+
+WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=60ff7feffe4519e5fae6666312880962'
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=32671043335d465f8c425d5d4f3791b4"
 
 @app.route('/')
 @app.route('/<publication>')
 def home():
+    # do work for the articles
     publication=request.args.get("publication")
     if not publication:
         publication=DEFAULTS['publication']
     articles = get_news(publication)
+
+    # do work for the city weather
     city = request.args.get('city')
     if not city:
         city = DEFAULTS['city']
     weather=get_weather(city)
-    return render_template('home.html', articles=articles, weather=weather, source=publication.upper())
+
+    # setup the to and from currency then get conversions
+    currency_from = request.args.get('currency_from')
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get('currency_to')
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from,currency_to)
+
+    return render_template('home.html', articles=articles, weather=weather, source=publication.upper(),
+                           currency_from=currency_from, currency_to=currency_to, rate=rate)
 
 
 
@@ -43,8 +62,8 @@ def get_news(query):
 
 
 def get_weather(query):
-    api_url='http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=60ff7feffe4519e5fae6666312880962'
-    query = urllib.quote(query)
+    api_url=WEATHER_URL
+    query = urllib2.quote(query)
     url = api_url.format(query)
     data = urllib2.urlopen(url).read()
     parsed = json.loads(data)
@@ -57,6 +76,12 @@ def get_weather(query):
                   }
         return weather
 
+def get_rate(frm, to):
+    all_currency = urllib2.urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate=parsed.get(frm.upper())
+    to_rate=parsed.get(to.upper())
+    return (to_rate/frm_rate, parsed.keys())
 
 if __name__ == '__main__':
     app.run(debug=True)
