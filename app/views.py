0a2bbd169629c3347395f72b361ee10111939e96
wadere <wadere@gmail.com>
2017-05-24 18:12:03 -0600

    update

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 import feedparser
+import os
 from flask import Flask
 from flask import render_template
+
+import jinja2
+jinja_environment = jinja2.Environment(autoescape=True,
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
 app = Flask(__name__, template_folder='template/')
 
@@ -14,10 +19,8 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return render_template('template/home.html',
-                           title=first_article.get('title'),
-                           published=first_article.get('published'),
-                           summary=first_article.get('summary'))
+    return render_template('home.html',
+                           articles=feed['entries'],source=feed)
 
 
 if __name__ == '__main__':
